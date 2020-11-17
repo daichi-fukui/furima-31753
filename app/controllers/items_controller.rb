a109b5ff41c965f019dde5f8c852ffa194910e49
daichi-fukui <fukui.daichi0913@icloud.com>
2020-11-17 16:34:55 +0900

    変数修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,14 +24,14 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless  current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
