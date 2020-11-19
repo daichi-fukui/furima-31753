5b150897ec3a6d2dc178f47194008d461150a88d
daichi-fukui <fukui.daichi0913@icloud.com>
2020-11-19 12:35:33 +0900

    フォームの情報保存機能、エラーメッセージ表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,30 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
-    
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :area_id, :city, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number)
+  end
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,18 +1,18 @@
 class OrderAddress
   
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :area, :city, :house_number, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]+\z/}
-    validates :area, numericality: { other_than: 1 }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :area_id, numericality: { other_than: 1 }
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :house_number, format: { with: /\A[a-z0-9]+\z/}
-    validates :phone_number, format: { with: /\A[0-9]+\z/}
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{1,11}\z/}
   end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, area: area, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: )
+    Address.create(postal_code: postal_code, area_id: area_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order_id )
   end
 end
