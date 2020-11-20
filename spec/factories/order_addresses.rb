b2ab042cbac9f0c47a3028e10e719aa547bc2ff7
daichi-fukui <fukui.daichi0913@icloud.com>
2020-11-20 18:55:17 +0900

    修正分

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -5,8 +5,10 @@ FactoryBot.define do
     area_id           {2}
     city              {"横浜"}
     house_number      {"青山1-1-1"}
+    building_name     {""}
     phone_number      {"09012345678"}
     token             {"sample_token"}
-
+    user_id           {2}
+    item_id           {2}
   end
 end
